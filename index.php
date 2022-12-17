16ef864645c478ed07ade7942f185498e31b8041
serko-99-ops <80706051+serko-99-ops@users.noreply.github.com>
2022-12-17 19:45:26 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+Sip_server = $_SERVER[‘SERVER ADDDR’];
+echo "<h1>Hello from Kubernetes</h1><br>";
+echo "Server IP Address is: $ip server", "<br><p>"
+echo "Made by  <font color=blue>Serega";
+?>
